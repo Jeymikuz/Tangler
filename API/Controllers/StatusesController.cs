82e08067b26a66ed3cd96baf450475b5bff50603
Piotr Marszałek <45011377+Jeymikuz@users.noreply.github.com>
2021-09-22 18:45:10 +0200

    Endpoint to get list of statuses

---
+++ API/Controllers/StatusesController.cs
@@ -0,0 +1,18 @@
+﻿using Application.Statuses;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class StatusesController : BaseApiController
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetStatuses()
+        {
+            return HandleResult(await Mediator.Send(new List.Query()));
+        }
+    }
+}
---
+++ Application/Statuses/List.cs
@@ -0,0 +1,41 @@
+﻿using Application.Core;
+using Application.Interfaces;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Statuses
+{
+    public static class List
+    {
+        public class Query : IRequest<Result<List<Status>>> { }
+
+        public class Handler : IRequestHandler<Query, Result<List<Status>>>
+        {
+            private readonly IUserAccessor _userAccessor;
+            private readonly DataContext _context;
+
+            public Handler(IUserAccessor userAccessor, DataContext context)
+            {
+                _userAccessor = userAccessor;
+                _context = context;
+            }
+
+            public async Task<Result<List<Status>>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var user = await _context.Users.Include(x=>x.Company).ThenInclude(s=>s.Statuses).SingleOrDefaultAsync(x => x.UserName == _userAccessor.GetUsername());
+
+                if (user is null) return Result<List<Status>>.Failure("User not found");
+
+                return Result<List<Status>>.Success(user.Company.Statuses.ToList());
+             }
+        }
+    }
+}
