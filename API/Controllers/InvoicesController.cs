6e1c118ba99953b0c1fbc1c4ba50f8f56d763619
Piotr Marszałek <marszalek.piotr1@gmail.com>
2022-01-15 20:20:30 +0100

    #3 Added APi and Front Invoices

---
+++ API/Controllers/InvoicesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Documents.Invoices.GenerateInvoice;
+using Application.Documents.Invoices.Get;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class InvoicesController : BaseApiController
+    {
+        [HttpPost("{orderId}")]
+        public async Task<IActionResult> GenerateInvoice([FromRoute] string orderId)
+        {
+            return HandleResult(await Mediator.Send(new CreateInvoiceCommand() {OrderId = orderId}));
+        }
+
+        [HttpGet("{orderId}")]
+        public async Task<IActionResult> GetInvoice([FromRoute] string orderId)
+        {
+            var data = await Mediator.Send(new GetInvoiceQuery() {OrderId = orderId , Context = HttpContext});
+
+            if (data is null) return BadRequest("Invoice not found");
+
+            var cd = new System.Net.Mime.ContentDisposition
+            {
+                // for example foo.bak
+                FileName = "Invoice.pdf",
+
+                // always prompt the user for downloading, set to true if you want 
+                // the browser to try to show the file inline
+                Inline = false,
+            };
+            Response.ContentType = "application/pdf";
+            //Response.Headers.Add("Content-Disposition", cd.ToString());
+
+            return File(data, "application/pdf");
+        }
+    }
+}
